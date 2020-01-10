c4a627ee881076f2eb28b28245a298a192465ea3
gernest <geofreyernest@live.com>
2020-01-10 23:39:00 +0300

    add TestIncludeReqular

---
+++ parse_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
+
+func TestIncludeReqular(t *testing.T) {
+	filename := "config/fixture/crossplane/includes-regular/nginx.conf"
+	p := parse(filename, defaultParseOpts())
+	b, _ := json.Marshal(p)
+	expect, err := ioutil.ReadFile(filepath.Join(filepath.Dir(filename), "expect.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(b, expect) {
+		t.Error("failed to match expectation")
+	}
+}
