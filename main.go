380b9af732b3b945caa3fa4d32477d555ac29fde
gernest <geofreyernest@live.com>
2020-01-14 18:01:59 +0300

    cleanup

--- main.go
+++ main.go
@@ -2,17 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-
-	"github.com/ergongate/vince/engine"
 )
 
 func main() {
 	flag.Parse()
-	err := engine.Run()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
 }
