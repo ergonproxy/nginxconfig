3157128bc7da52731bab83c3cc5f80fc52eb6303
gernest <geofreyernest@live.com>
2020-02-02 15:46:51 +0300

    add validURL tests

--- oauth2_server_test.go
+++ oauth2_server_test.go
@@ -51,4 +51,62 @@ func TestOauth2(t *testing.T) {
 			}
 		}
 	})
+	t.Run("ValidURL", func(t *testing.T) {
+		link := "http://www.example.com"
+		sample := []struct {
+			info, base, redir string
+			valid             bool
+		}{
+			{"exact match", "/vince", "/vince", true},
+			{"trailing slash", "/vince", "/vince/", true},
+			{"exact match with trailing slash", "/vince/", "/vince/", true},
+			{"subpath", "/vince", "/vince/sub/path", true},
+			{"subpath with trailing slash", "/vince/", "/vince/sub/path", true},
+			{"subpath with traversal like", "/vince", "/vince/.../..sub../...", true},
+			{"traversal", "/vince/../allow", "/vince/../allow/sub/path", true},
+			{"base path mismatch", "/vince", "/vinceine", false},
+			{"base path mismatch slash", "/vince/", "/vince", false},
+			{"traversal", "/vince", "/vince/..", false},
+			{"embed traversal", "/vince", "/vince/../sub", false},
+			{"not subpath", "/vince", "/vince../sub", false},
+		}
+
+		for _, v := range sample {
+			if v.valid {
+				err := validateURI(link+v.base, link+v.redir)
+				if err != nil {
+					t.Errorf("some fish for %s : %v", v.info, err)
+				}
+			} else {
+				err := validateURI(link+v.base, link+v.redir)
+				if err == nil {
+					t.Errorf("expected error for for %s : got %v", v.info, err)
+				}
+			}
+		}
+
+		sampleList := []struct {
+			base, redir, sep string
+			valid            bool
+		}{
+			{"http://www.example.com/vince", "http://www.example.com/vince", "", true},
+			{"http://www.example.com/vince", "http://www.example.com/app", "", false},
+			{"http://xxx:14000/vince;http://www.example.com/vince", "http://www.example.com/vince", ";", true},
+			{"http://xxx:14000/vince;http://www.example.com/vince", "http://www.example.com/app", ";", false},
+		}
+
+		for _, v := range sampleList {
+			if v.valid {
+				err := validateURIList(v.base, v.redir, v.sep)
+				if err != nil {
+					t.Error(err)
+				}
+			} else {
+				err := validateURIList(v.base, v.redir, v.sep)
+				if err == nil {
+					t.Error("expected an error")
+				}
+			}
+		}
+	})
 }
