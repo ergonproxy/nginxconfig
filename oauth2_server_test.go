ca009fce27ea0d2dd309e0a874352a4037252b1d
gernest <geofreyernest@live.com>
2020-02-02 15:44:02 +0300

    refactor oauth test

--- oauth2_server_test.go
+++ oauth2_server_test.go
@@ -6,32 +6,33 @@ import (
 	"testing"
 )
 
-func TestOauth2Context(t *testing.T) {
-	w := httptest.NewRecorder()
-	var ctx oauth2Context
-	ctx.init()
-	link := "http://www.example.com"
-
-	ctx.setErrURI(oauth2ErrInvalidClient, "", link, "")
-	ctx.setRedirect(link)
-	rdir, err := ctx.getRedirectURL()
-	if err != nil {
-		t.Error(err)
-	}
-	if !strings.Contains(rdir, link+"?") {
-		t.Errorf("expected a normal query got %s", rdir)
-	}
-	ctx.redirectInFragment = true
-	rdir, err = ctx.getRedirectURL()
-	if err != nil {
-		t.Error(err)
-	}
-	if !strings.Contains(rdir, link+"#") {
-		t.Errorf("expected a normal query got %s", rdir)
-	}
-	ctx.kind = oauth2ResponseData
-	err = ctx.commit(w)
-	if err != nil {
-		t.Error(err)
-	}
+func TestOauth2(t *testing.T) {
+	t.Run("Context", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		var ctx oauth2Context
+		ctx.init()
+		link := "http://www.example.com"
+		ctx.setErrURI(oauth2ErrInvalidClient, "", link, "")
+		ctx.setRedirect(link)
+		rdir, err := ctx.getRedirectURL()
+		if err != nil {
+			t.Error(err)
+		}
+		if !strings.Contains(rdir, link+"?") {
+			t.Errorf("expected a normal query got %s", rdir)
+		}
+		ctx.redirectInFragment = true
+		rdir, err = ctx.getRedirectURL()
+		if err != nil {
+			t.Error(err)
+		}
+		if !strings.Contains(rdir, link+"#") {
+			t.Errorf("expected a normal query got %s", rdir)
+		}
+		ctx.kind = oauth2ResponseData
+		err = ctx.commit(w)
+		if err != nil {
+			t.Error(err)
+		}
+	})
 }
