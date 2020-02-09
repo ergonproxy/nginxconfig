d8720a2406b4bdedd611a75e387f4768254414bb
gernest <geofreyernest@live.com>
2020-02-09 10:21:08 +0300

    add logFormat

--- ngx_logger.go
+++ ngx_logger.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strings"
 	"sync"
 	"time"
 
@@ -16,6 +17,35 @@ type (
 )
 
 const defaultLogFormat = `$remote_addr - $remote_user [$time_local] "$request" $status $body_bytes_sent "$http_referer" "$http_user_agent"`
+
+type logFormat struct {
+	name     string
+	template string
+	escape   string
+}
+
+func (l *logFormat) defaults() {
+	l.name = "combined"
+	l.template = defaultLogFormat
+	l.escape = "default"
+}
+
+func (l *logFormat) load(r *rule) {
+	if len(r.args) > 0 {
+		l.name = r.args[0]
+		if len(r.args) > 1 {
+			switch r.args[1] {
+			case "default", "json", "none":
+				l.escape = r.args[1]
+				if len(r.args) > 2 {
+					l.template = strings.Join(r.args[2:], "")
+				}
+			default:
+				l.template = strings.Join(r.args[1:], "")
+			}
+		}
+	}
+}
 
 type ngxLogger interface {
 	Println(file string, level string, message []byte)
