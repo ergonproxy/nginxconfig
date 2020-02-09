94b04fd90f0fdafecf8e95787193446452ffb107
gernest <geofreyernest@live.com>
2020-02-09 10:05:24 +0300

    manage file cache
    remove unused files and files that don't exist

--- access_log.go
+++ access_log.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"container/list"
+	"context"
 	"errors"
 	"net/http"
 	"os"
@@ -23,6 +24,7 @@ type fileLogCacheOption struct {
 	max      intValue
 	inactive durationValue
 	valid    durationValue
+	min      intValue
 }
 
 func (o *fileLogCacheOption) defaults() {
@@ -30,9 +32,13 @@ func (o *fileLogCacheOption) defaults() {
 	o.max.store(100)
 	o.inactive.store(10 * time.Second)
 	o.valid.store(60 * time.Second)
+	o.min.store(1)
 }
 
 func (f *fileCache) deleteUnsafe(node *list.Element) {
+	if node == nil {
+		return
+	}
 	v := node.Value.(*list.Element).Value.(*fileObject)
 	v.file.Close()
 	delete(f.hash, v.path)
@@ -40,13 +46,59 @@ func (f *fileCache) deleteUnsafe(node *list.Element) {
 	f.list.Remove(node)
 }
 
-func (f *fileCache) init(opts fileLogCacheOption) bool {
+func (f *fileCache) manageKeys(ctx context.Context) {
+	valid := time.NewTicker(f.opts.valid.value)
+	defer valid.Stop()
+	inactive := time.NewTicker(f.opts.inactive.value)
+	defer inactive.Stop()
+	min := f.opts.min.value
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-inactive.C:
+			var junk []string
+			f.uses.Range(func(key, value interface{}) bool {
+				if value.(int64) < min {
+					junk = append(junk, key.(string))
+					f.uses.Delete(key)
+				}
+				return true
+			})
+			f.Delete(junk...)
+		case <-valid.C:
+			var junk []string
+			f.uses.Range(func(key, value interface{}) bool {
+				_, err := os.Stat(key.(string))
+				if os.IsNotExist(err) {
+					junk = append(junk, key.(string))
+					f.uses.Delete(key)
+				}
+				return true
+			})
+			f.Delete(junk...)
+		}
+	}
+}
+
+func (f *fileCache) Delete(keys ...string) {
+	if len(keys) > 0 {
+		f.mu.Lock()
+		for _, key := range keys {
+			f.deleteUnsafe(f.hash[key])
+		}
+		f.mu.Unlock()
+	}
+}
+
+func (f *fileCache) init(ctx context.Context, opts fileLogCacheOption) bool {
 	if !opts.on.value {
 		return false
 	}
 	f.list = new(list.List)
 	f.uses = new(sync.Map)
 	f.hash = make(map[string]*list.Element)
+	go f.manageKeys(ctx)
 	return true
 }
 
